4c151c09251768638f934de296ef7970aa8b7f95
vv1ck <75469718+vv1ck@users.noreply.github.com>
2021-09-28 18:33:28 -0400

    Create start.py

---
+++ start.py
@@ -0,0 +1,17 @@
+import os
+try:
+    import requests
+except ModuleNotFoundError:
+    os.system('pip install requests')
+    os.system('clear')
+try:
+    import time
+except ModuleNotFoundError:
+    os.system('pip install time')
+    os.system('clear')
+try:
+    import threading
+except ModuleNotFoundError:
+    os.system('pip install threading')
+    os.system('clear')
+os.system('bash r00t.sh')
